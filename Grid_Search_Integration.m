c312ddd4733b2d10f366aeee4dd7a53c4c154f66
JochemBeurskens <81189783+JochemBeurskens@users.noreply.github.com>
2022-04-26 18:07:45 +0200

    added C=2, and normalisation

--- Grid_Search_Integration.m
+++ Grid_Search_Integration.m
@@ -34,8 +34,16 @@ L_av_xv=zeros(2*n-1,2*n-1,t_max);
 M_av_xa=zeros(2*n-1,2*n-1,t_max);
 M_av_xv=zeros(2*n-1,2*n-1,t_max);
 
+L_av_xa_c2=zeros(2*n-1,2*n-1,t_max);
+L_av_xv_c2=zeros(2*n-1,2*n-1,t_max);
+M_av_xa_c2=zeros(2*n-1,2*n-1,t_max);
+M_av_xv_c2=zeros(2*n-1,2*n-1,t_max);
+
 P_av_a=zeros(2*n-1,2*n-1,t_max);
 P_av_v=zeros(2*n-1,2*n-1,t_max);
+
+P_av_a_c2=zeros(2*n-1,2*n-1,t_max);
+P_av_v_c2=zeros(2*n-1,2*n-1,t_max);
 %parameters for C=1
 sig_e_s=0.1;
 sig_l_s=0.6;
@@ -71,6 +79,10 @@ sig_mv_c2=1.0; %setting the noise parameters for the subject's observation
 for ls=1:length(lsteps)-1
     L(ls)=(normcdf(lsteps(ls+1),0,sig_l_s)-normcdf(lsteps(ls),0,sig_l_s));
     M(ls)=(normcdf(lsteps(ls+1),0,sig_m_s)-normcdf(lsteps(ls),0,sig_m_s));
+    L_a_c2(ls)=(normcdf(lsteps(ls+1),-2,sig_l_as_c2)-normcdf(lsteps(ls),-2,sig_l_as_c2));
+    M_a_c2(ls)=(normcdf(lsteps(ls+1),-2,sig_m_as_c2)-normcdf(lsteps(ls),-2,sig_m_as_c2));
+    L_v_c2(ls)=(normcdf(lsteps(ls+1),2,sig_l_vs_c2)-normcdf(lsteps(ls),2,sig_l_vs_c2));
+    M_v_c2(ls)=(normcdf(lsteps(ls+1),2,sig_m_vs_c2)-normcdf(lsteps(ls),2,sig_m_vs_c2));
 end
 %% Performing the grid search
 store_percept_av=zeros(n,n,t_max);
@@ -84,53 +96,132 @@ for t=1:t_max
    
     li=1;
     %C=1
-    for li=1:repeat_l
-        mi=1;
-        for mi=1:repeat_m    
-            f_v_s_store=zeros(repeat,t_max);
-            f_a_s_store=zeros(repeat,t_max);
-            L_av_s=l(li);
-            M_av_s=m(mi);
+    for li=1:repeat_l %looping over all location grid points
+        %mi=1;
+        for mi=1:repeat_m %looping over all semantic meaning grid points
+            L_av_s=l(li); 
+            M_av_s=m(mi); %placing the centre of the stimuli at each of the grid points
             %C=1, run model and plot
-            for ls =1:length(lsteps)-1
+            for ls =1:length(lsteps)-1 %now for each centre of the stimuli the range of observations is looked at
                 for ms =1:length(msteps)-1
                     L_av_xa(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_s,sig_la)-normcdf(lsteps(ls),L_av_s,sig_la));
                     L_av_xv(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_s,sig_lv)-normcdf(lsteps(ls),L_av_s,sig_lv));
                     M_av_xa(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(msteps(ms+1),M_av_s,sig_ma)-normcdf(msteps(ms),M_av_s,sig_ma));
                     M_av_xv(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(msteps(ms+1),M_av_s,sig_mv)-normcdf(msteps(ms),M_av_s,sig_mv));
+                    %the above calcualtes the spread of the observations
+                    %given each of the locations for the centres in the
+                    %grid. By summing the probabilities distributions of
+                    %the observations, for all possible stimulus locations
+                    %the integral is performed in a numerical fashion. Note
+                    %that the multiplication with the prior probabilities
+                    %for the stimulus locations has been postponed to line
+                    %115, so that the code is less clutered. But as
+                    %associativity holds for sums of real numbers, this
+                    %shouldn't influence the result (i.e. C*a+C*b=C(a+b)).
                 end
             end
-            mi=mi+1;
+            %mi=mi+1;
         end
-        li=li+1;
+        %li=li+1;
     end
     if t==1
-        P_av_a(:,:,t)=P_C*(L_av_xa(:,:,t)*+M_av_xa(:,:,t));
-        P_av_v(:,:,t)=P_C*(L_av_xv(:,:,t)+M_av_xv(:,:,t));
+        P_av_a(:,:,t)=P_C*L.*((L_av_xa(:,:,t)*+M_av_xa(:,:,t))).*M';
+        P_av_v(:,:,t)=P_C*L.*((L_av_xv(:,:,t)+M_av_xv(:,:,t))).*M';
     else 
-        P_av_a(:,:,t)=P_av_v(:,:,t-1)*P_C*(L_av_xa(:,:,t)+M_av_xa(:,:,t));
-        P_av_v(:,:,t)=P_av_v(:,:,t-1)*P_C*(L_av_xv(:,:,t)+M_av_xv(:,:,t));
+        P_av_a(:,:,t)=P_av_a(:,:,t-1)*(P_C*L.*((L_av_xa(:,:,t)+M_av_xa(:,:,t))).*M');
+        P_av_v(:,:,t)=P_av_v(:,:,t-1)*(P_C*L.*((L_av_xv(:,:,t)+M_av_xv(:,:,t))).*M');
+        %here the probability distributions for the locations and the
+        %semantic meanings are added, and then multiplied with the prior
+        %probabilities for the stimulus locations. The update step is also
+        %performed, thus multiplying the probabilities for the current time
+        %step with those for the previous time step. At this point all that
+        %rests is normalisation but further the integration has been
+        %performed.
+    end
+    %C=2
+    %in this case each possible visual stimulus location has to be matched
+    %to a possible auditory stimulus location, creating a double for loop
+    %over the stimulus locations, but the inner for loops for estimating
+    %the probability at a observation location can remain as it doesn't
+    %matter where the stimulus is located due to the observation
+    %probability being investigated at each and every possible grid point
+    %anayways.
+    for li_a=1:repeat_l %looping over all location grid points
+        %mi_a=1;
+        for mi_a=1:repeat_m %looping over all semantic meaning grid points
+            %li_v=1;
+            for li_v=1:repeat_l %looping over all location grid points
+                %mi_v=1;
+                for mi_v=1:repeat_m
+                    L_av_sv=l(li_v); 
+                    M_av_sv=m(mi_v);
+                    L_av_sa=l(li_a); 
+                    M_av_sa=m(mi_a); %placing the centre of the stimuli at each of the grid points
+                    %C=1, run model and plot
+                    for ls =1:length(lsteps)-1 %now for each centre of the stimuli the range of observations is looked at
+                        for ms =1:length(msteps)-1
+                            L_av_xa_c2(ls,ms,t)=L_av_xa_c2(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_sa,sig_la_c2)-normcdf(lsteps(ls),L_av_sa,sig_la_c2));
+                            L_av_xv_c2(ls,ms,t)=L_av_xa_c2(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_sv,sig_lv_c2)-normcdf(lsteps(ls),L_av_sv,sig_lv_c2));
+                            M_av_xa_c2(ls,ms,t)=M_av_xa_c2(ls,ms,t)+(normcdf(msteps(ms+1),M_av_sa,sig_ma_c2)-normcdf(msteps(ms),M_av_sa,sig_ma_c2));
+                            M_av_xv_c2(ls,ms,t)=M_av_xa_c2(ls,ms,t)+(normcdf(msteps(ms+1),M_av_sv,sig_mv_c2)-normcdf(msteps(ms),M_av_sv,sig_mv_c2));
+                            %the above calcualtes the spread of the observations
+                            %given each of the locations for the centres in the
+                            %grid. By summing the probabilities distributions of
+                            %the observations, for all possible stimulus locations
+                            %the integral is performed in a numerical fashion. Note
+                            %that the multiplication with the prior probabilities
+                            %for the stimulus locations has been postponed to line
+                            %115, so that the code is less clutered. But as
+                            %associativity holds for sums of real numbers, this
+                            %shouldn't influence the result (i.e. C*a+C*b=C(a+b)).
+                        end
+                    end
+                    %mi_v=mi_v+1;
+                end
+                %li_v=li_v+1;
+            end
+            %mi_a=mi_a+1;
+        end
+        %li_a=li_a+1;
+    end
+    if t==1
+        P_av_a_c2(:,:,t)=P_C*L_a_c2.*((L_av_xa_c2(:,:,t)*+M_av_xa_c2(:,:,t))).*M_a_c2';
+        P_av_v_c2(:,:,t)=P_C*L_v_c2.*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t))).*M_v_c2';
+    else 
+        P_av_a_c2(:,:,t)=P_av_a_c2(:,:,t-1)*(P_C*L_a_c2.*((L_av_xa_c2(:,:,t)+M_av_xa_c2(:,:,t))).*M_a_c2');
+        P_av_v_c2(:,:,t)=P_av_v_c2(:,:,t-1)*(P_C*L_v_c2.*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t))).*M_v_c2');
+        %here the probability distributions for the locations and the
+        %semantic meanings are added, and then multiplied with the prior
+        %probabilities for the stimulus locations. The update step is also
+        %performed, thus multiplying the probabilities for the current time
+        %step with those for the previous time step. At this point all that
+        %rests is normalisation but further the integration has been
+        %performed.
     end
 end
 
-%% Plotting
-P_av_a=L.*P_av_a.*M';%multiplication with the prior distributions
-P_av_v=L.*P_av_v.*M';
+%% Normalisation and plotting
+for o=1:t_max
+    P_av_v(:,:,o)=P_av_v(:,:,t)/sum(sum(P_av_v(:,:,t)));
+    P_av_a(:,:,o)=P_av_a(:,:,t)/sum(sum(P_av_a(:,:,t))); %normalising the probability distributions
+    P_av_v_c2(:,:,o)=P_av_v_c2(:,:,o)/sum(sum(P_av_v_c2(:,:,o)));
+    P_av_a_c2(:,:,o)=P_av_a_c2(:,:,o)/sum(sum(P_av_a_c2(:,:,o)));
+end
 [x,y]=meshgrid(lsteps_plt,msteps_plt);
 %first the results from the update equation
 %C=1: first the audio-visual
 figure(1)
-contour(x,y,P_av_a(:,:,1),'ShowText','on')
+contour(x,y,P_av_a_c2(:,:,1),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
 figure(2)
-contour(x,y,P_av_a(:,:,2),'ShowText','on')
+contour(x,y,P_av_a_c2(:,:,2),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
 figure(3)
-contour(x,y,P_av_a(:,:,3),'ShowText','on')
+contour(x,y,P_av_a_c2(:,:,3),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
