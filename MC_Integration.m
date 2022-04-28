dea8be3b8fc1915d36dddb49a3c1000583acb062
JochemBeurskens <81189783+JochemBeurskens@users.noreply.github.com>
2022-04-28 18:09:10 +0200

    added Monte Carlo integration, updated grid search

--- MC_Integration.m
+++ MC_Integration.m
@@ -10,8 +10,8 @@ P_av_given_LMf=zeros(n,n,n,t_max); %make an array to store the probabilities in,
 P_a_given_LMf_c2=zeros(n,n,n,t_max);%same but for the C=2 and the auditory percept
 P_v_given_LMf_c2=zeros(n,n,n,t_max);%same but for the C=2 and the visual percept
 
-repeat_l=n;
-repeat_m=n;
+repeat_l=20;
+repeat_m=20;
 repeat=1;
 Percept_L_av=zeros(n,n,repeat); %make arrays to store the percepts for all the repeated trials, note that there will be n,n,repeat entries (n draws from the space of l, for each draw of l 5 draws from m follow, then 10000 times repeated draws for those positions in m,l space)
 Percept_M_av=zeros(n,n,repeat);
@@ -77,13 +77,17 @@ sig_mv_c2=1.0; %setting the noise parameters for the subject's observation
 
 %% obtain the prior distributions
 for ls=1:length(lsteps)-1
-    L(ls)=(normcdf(lsteps(ls+1),0,sig_l_s)-normcdf(lsteps(ls),0,sig_l_s));%*(normcdf(lsteps(ls+1),L_av_s,sig_la)-normcdf(lsteps(ls),L_av_s,sig_la));
-    M(ls)=(normcdf(lsteps(ls+1),0,sig_m_s)-normcdf(lsteps(ls),0,sig_m_s));%*(normcdf(msteps(ls+1),M_av_s,sig_ma)-normcdf(msteps(ls),M_av_s,sig_ma));
+    L(ls)=(normcdf(lsteps(ls+1),0,sig_l_s)-normcdf(lsteps(ls),0,sig_l_s));
+    M(ls)=(normcdf(lsteps(ls+1),0,sig_m_s)-normcdf(lsteps(ls),0,sig_m_s));
     L_a_c2(ls)=(normcdf(lsteps(ls+1),-2,sig_l_as_c2)-normcdf(lsteps(ls),-2,sig_l_as_c2));
     M_a_c2(ls)=(normcdf(lsteps(ls+1),-2,sig_m_as_c2)-normcdf(lsteps(ls),-2,sig_m_as_c2));
     L_v_c2(ls)=(normcdf(lsteps(ls+1),2,sig_l_vs_c2)-normcdf(lsteps(ls),2,sig_l_vs_c2));
     M_v_c2(ls)=(normcdf(lsteps(ls+1),2,sig_m_vs_c2)-normcdf(lsteps(ls),2,sig_m_vs_c2));
 end
+%In this case we no longer need to multiply with the prior distribution
+%explicitly, as algorithm 1 of A tutorial on particle filters by Maarten Speekenbrink
+%shows that obtaining integral(f(y)*p(y)dy) can be obtained by sampling
+%from p(y) and evaluating f(y) at those locations.y
 %% Performing the grid search
 store_percept_av=zeros(n,n,t_max);
 store_percept_a=zeros(n,n,t_max);
@@ -99,19 +103,15 @@ for t=1:t_max
     for li=1:repeat_l %looping over all location grid points
         %mi=1;
         for mi=1:repeat_m %looping over all semantic meaning grid points
-            L_av_s=l(li); 
-            M_av_s=m(mi); %placing the centre of the stimuli at each of the grid points
+            M_av_s=normrnd(0.0,sig_m_s); 
+            L_av_s=normrnd(0.0,sig_l_s); %drawing the M and L from the probability distributions for these parameters, this should simulate the effect of multiplication with the P(L_{av}^{s}) etc.
             %C=1, run model and plot
             for ls =1:length(lsteps)-1 %now for each centre of the stimuli the range of observations is looked at
                 for ms =1:length(msteps)-1
-                    L_av_xa(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),0,sig_l_s)-normcdf(lsteps(ls),0,sig_l_s))*(normcdf(lsteps(ls+1),L_av_s,sig_la)-normcdf(lsteps(ls),L_av_s,sig_la));
-%                     (normcdf(lsteps(ls+1),L_av_s,sig_la)-normcdf(lsteps(ls),L_av_s,sig_la));
-                    L_av_xv(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),0,sig_l_s)-normcdf(lsteps(ls),0,sig_l_s))*(normcdf(lsteps(ls+1),L_av_s,sig_lv)-normcdf(lsteps(ls),L_av_s,sig_lv));
-%                     (normcdf(lsteps(ls+1),L_av_s,sig_lv)-normcdf(lsteps(ls),L_av_s,sig_lv));
-                    M_av_xa(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(lsteps(ms+1),0,sig_m_s)-normcdf(lsteps(ms),0,sig_m_s))*(normcdf(msteps(ms+1),M_av_s,sig_ma)-normcdf(msteps(ms),M_av_s,sig_ma));
-%                     (normcdf(msteps(ms+1),M_av_s,sig_ma)-normcdf(msteps(ms),M_av_s,sig_ma));
-                    M_av_xv(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(lsteps(ms+1),0,sig_m_s)-normcdf(lsteps(ms),0,sig_m_s))*(normcdf(msteps(ms+1),M_av_s,sig_mv)-normcdf(msteps(ms),M_av_s,sig_mv));
-%                     (normcdf(msteps(ms+1),M_av_s,sig_mv)-normcdf(msteps(ms),M_av_s,sig_mv));
+                    L_av_xa(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_s,sig_la)-normcdf(lsteps(ls),L_av_s,sig_la));
+                    L_av_xv(ls,ms,t)=L_av_xa(ls,ms,t)+(normcdf(lsteps(ls+1),L_av_s,sig_lv)-normcdf(lsteps(ls),L_av_s,sig_lv));
+                    M_av_xa(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(msteps(ms+1),M_av_s,sig_ma)-normcdf(msteps(ms),M_av_s,sig_ma));
+                    M_av_xv(ls,ms,t)=M_av_xa(ls,ms,t)+(normcdf(msteps(ms+1),M_av_s,sig_mv)-normcdf(msteps(ms),M_av_s,sig_mv));
                     %the above calcualtes the spread of the observations
                     %given each of the locations for the centres in the
                     %grid. By summing the probabilities distributions of
@@ -131,13 +131,9 @@ for t=1:t_max
     if t==1
         P_av_a(:,:,t)=P_C*((L_av_xa(:,:,t)+M_av_xa(:,:,t)));
         P_av_v(:,:,t)=P_C*((L_av_xv(:,:,t)+M_av_xv(:,:,t)));
-%         P_av_a(:,:,t)=P_C*L.*((L_av_xa(:,:,t)+M_av_xa(:,:,t))).*M';
-%         P_av_v(:,:,t)=P_C*L.*((L_av_xv(:,:,t)+M_av_xv(:,:,t))).*M';
     else 
         P_av_a(:,:,t)=P_av_a(:,:,t-1).*(P_C*((L_av_xa(:,:,t)+M_av_xa(:,:,t))));
         P_av_v(:,:,t)=P_av_v(:,:,t-1).*(P_C*((L_av_xv(:,:,t)+M_av_xv(:,:,t))));
-%         P_av_a(:,:,t)=P_av_a(:,:,t-1).*(P_C*L.*((L_av_xa(:,:,t)+M_av_xa(:,:,t))).*M');
-%         P_av_v(:,:,t)=P_av_v(:,:,t-1).*(P_C*L.*((L_av_xv(:,:,t)+M_av_xv(:,:,t))).*M');
         %here the probability distributions for the locations and the
         %semantic meanings are added, and then multiplied with the prior
         %probabilities for the stimulus locations. The update step is also
@@ -161,10 +157,10 @@ for t=1:t_max
 %             for li_v=1:repeat_l %looping over all location grid points
 %                 %mi_v=1;
 %                 for mi_v=1:repeat_m
-%                     L_av_sv=l(li_v); 
-%                     M_av_sv=m(mi_v);
-%                     L_av_sa=l(li_a); 
-%                     M_av_sa=m(mi_a); %placing the centre of the stimuli at each of the grid points
+%                     M_a=normrnd(0.25,sig_m_as_c2); 
+%                     M_v=normrnd(-1.25,sig_m_vs_c2);
+%                     L_a=normrnd(-0.25,sig_l_as_c2);
+%                     L_v=normrnd(1.25,sig_l_vs_c2); %drawing the M and L from the probability distributions for these parameters, this should simulate the effect of multiplication with the P(L_{av}^{s}) etc.
 %                     %C=1, run model and plot
 %                     for ls =1:length(lsteps)-1 %now for each centre of the stimuli the range of observations is looked at
 %                         for ms =1:length(msteps)-1
@@ -193,11 +189,11 @@ for t=1:t_max
 %         %li_a=li_a+1;
 %     end
 %     if t==1
-%         P_av_a_c2(:,:,t)=P_C*L_a_c2.*((L_av_xa_c2(:,:,t)*+M_av_xa_c2(:,:,t))).*M_a_c2';
-%         P_av_v_c2(:,:,t)=P_C*L_v_c2.*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t))).*M_v_c2';
+%         P_av_a_c2(:,:,t)=P_C*((L_av_xa_c2(:,:,t)*+M_av_xa_c2(:,:,t)));
+%         P_av_v_c2(:,:,t)=P_C*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t)));
 %     else 
-%         P_av_a_c2(:,:,t)=P_av_a_c2(:,:,t-1)*(P_C*L_a_c2.*((L_av_xa_c2(:,:,t)+M_av_xa_c2(:,:,t))).*M_a_c2');
-%         P_av_v_c2(:,:,t)=P_av_v_c2(:,:,t-1)*(P_C*L_v_c2.*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t))).*M_v_c2');
+%         P_av_a_c2(:,:,t)=P_av_a_c2(:,:,t-1)*(P_C*((L_av_xa_c2(:,:,t)+M_av_xa_c2(:,:,t))));
+%         P_av_v_c2(:,:,t)=P_av_v_c2(:,:,t-1)*(P_C*((L_av_xv_c2(:,:,t)+M_av_xv_c2(:,:,t))));
 %         %here the probability distributions for the locations and the
 %         %semantic meanings are added, and then multiplied with the prior
 %         %probabilities for the stimulus locations. The update step is also
@@ -219,17 +215,17 @@ end
 %first the results from the update equation
 %C=1: first the audio-visual
 figure(1)
-contour(x,y,P_av_v(:,:,1),'ShowText','on')
+contour(x,y,P_av_a(:,:,1),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
 figure(2)
-contour(x,y,P_av_v(:,:,2),'ShowText','on')
+contour(x,y,P_av_a(:,:,2),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
 figure(3)
-contour(x,y,P_av_v(:,:,10),'ShowText','on')
+contour(x,y,P_av_a(:,:,10),'ShowText','on')
 hold on;
 xlabel("Location")
 ylabel("Semantic meaning")
