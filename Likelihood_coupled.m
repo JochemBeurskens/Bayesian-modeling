f54849e96c1b71deb1589840c4d550514a3a72c6
JochemBeurskens <81189783+JochemBeurskens@users.noreply.github.com>
2023-03-09 17:48:06 +0100

    updates, new model

--- Likelihood_coupled.m
+++ Likelihood_coupled.m
@@ -1,21 +1,27 @@
 clear all;
 clc;
-rng(6); %setting the random number generator seed, for reproducible results
+% rng(3); %setting the random number generator seed, for reproducible results
 
 %% parameter specification
 t_max=1; %set number of time steps in the process
-n=20; %maybe not discritize, set the number of possible locations, the larger the more accurate, but also the more computation time is required
+n=50; %maybe not discritize, set the number of possible locations, the larger the more accurate, but also the more computation time is required
+m_n=4;
 n_r=1; %number of stes of a_x or v_x obtained for each set of L_s, M_s and c_s
-n_lm=1000; %number of sets of L^x and M^x that are considered for the integrals
+n_lm=1; %number of sets of L^x and M^x that are considered for the integrals
 c=0.8; %setting a parameter for the resampling, as this is done when N_eff<=c*n, setting c=1 means at every step, setting c=0 means you never resample
 k=1; %setting the multiplication for the linear functions
-lrange=[-20 20]; %the range of l, actual locations are between -10.5 and 10.5
-l=linspace(lrange(1),lrange(2),n); %setting the range of locations and meanings
-sig_l_s=5;
-sig_l_vs=5;
+l_r=40;
+lrange=[-l_r/2 l_r/2]; %the range of l, actual locations are between -10.5 and 10.5
+l=linspace(lrange(1),lrange(2),n);%lrange;%linspace(1,n,n); %setting the range of locations and meanings
+m_r=2;
+mrange=[-m_r m_r]; %the range of l, actual locations are between -10.5 and 10.5
+m=linspace(mrange(1),mrange(2),m_n); %setting the range of locations and meanings
+
+sig_l_s=1;
+sig_l_vs=1;
 sig_l_as=sig_l_vs; %when assuming that the original location is drawn from one normal distribution, only one sigma_L^s is needed, same for c. They can now be seperate draws though, which for C=1 was not possible
-sig_lax=8;
-sig_lvx=5;
+sig_lax=1;
+sig_lvx=1;
 sig_e_s=1;
 sig_e_ax=1;
 sig_e_vx=1;
@@ -23,50 +29,167 @@ sig_m_s=1;
 sig_m_as=1;
 sig_m_vs=sig_m_as;
 sig_m_ax=1;
-sig_m_vx=0.5;
+sig_m_vx=1;
 p_c=0.5; %if x param > p_c: m=1, else m=2
 p_h=1;
-mean_rw=2; %setting the mean of the random walk
-%now we only need to obtain 1 set of a^x and v^x, and then evaluate it all
-%from the perspective of the different L^s and M^s values in the grid
+mean_rw=10; %setting the mean of the random walk
 %% obtaining the a_x, v_x values
-[i_la_plt,i_lv_plt,m_a,m_v,a_x,v_x,i_l,c_av_s,m_av_xa,m_av_xv,m_s]=gen_model_full(k,t_max,n,l,sig_l_s,sig_lvx,sig_lax,sig_m_s,sig_m_as,sig_m_vs,sig_m_ax,sig_m_vx,sig_e_s,sig_e_ax,sig_e_vx,p_c,p_h,n_r,n_lm,mean_rw);
+% [i_la_plt,i_lv_plt,m_a,m_v,a_x,v_x,i_l,c_av_s,m_av_xa,m_av_xv,m_s]=gen_model_full(k,t_max,l,m,sig_l_s,sig_lvx,sig_lax,sig_m_s,sig_m_as,sig_m_vs,sig_m_ax,sig_m_vx,sig_e_s,sig_e_ax,sig_e_vx,p_c,p_h,n_r,n_lm,mean_rw);
+a_x=[   -1.9161   -0.1085    0.5103   -1.6292
+    1.0938    0.0892    0.4049    0.9844
+    0.9981   -1.4256    1.8917   -0.9620
+    0.8695   -1.1801    0.1592    1.4307
+    0.4572    0.2975    0.2789    0.7485
+    1.4196   -0.7699    0.3905   -1.6745
+    1.4214   -0.1926   -0.2686    1.5724
+    1.2844    0.8921    0.9381    0.6117
+    0.1610   -0.5772    0.6329    0.0262
+    0.7651   -3.3331   -1.9541    0.2779
+   -0.6937    0.5940   -0.7639    0.6489
+    0.7623   -2.1806   -1.5235    0.4395
+   -0.1942   -0.6217   -0.3277   -0.3331
+    0.1585    0.0913   -0.4925    1.1969
+   -2.8892   -0.6512   -1.5526    1.3257
+   -0.8330    0.4425    1.2924    0.1804
+   -0.0976    0.6613   -0.2145    0.6539
+    0.2258   -0.3354   -0.0225   -1.1352
+    0.7087   -0.1652    1.4996    0.0943
+    1.5971    3.4140   -0.9782   -2.2866
+    0.1959   -0.0465   -0.6613    0.9510
+    0.7182    0.8696    0.4592    1.5024
+   -0.5838   -0.9141   -0.2967    0.5192
+    0.4739   -1.6043   -0.7773   -1.0583
+    0.0582   11.8533    0.5037    1.3847
+    1.1070    0.7473   -0.8766    0.5830
+    0.0844   -1.0890   -1.9064   -0.6763
+   -2.4617   -2.1572   -0.5018   -0.5674
+    0.6605   -0.5471   -0.0163   -1.3161
+    0.3431    1.3117   -0.8473    1.0041
+    0.1683   -0.2427   -1.5496    0.8436
+    1.8458   -0.0254    0.3465   -0.5012
+    0.9162   -0.9471    0.3484    0.2725
+   -0.3857   -0.8793   -1.3805    0.0452
+   -0.1856   -2.0659   -0.9295    1.2524
+   -1.0100   -1.0801   -0.5971   -1.0798
+    0.2539   -0.3938   -1.5369   -0.6221
+   -1.7619    0.6442   -1.4600   -0.8555
+    0.0402   -0.7911    0.5240    0.3886
+    1.2653    2.0725   -0.6216    1.2173
+    0.3737    1.4913   -0.5762   -1.2599
+   -0.1230   -0.5632    0.6065    0.3262
+   -1.0139   -0.9937    1.4928    0.1216
+   -1.4826   -1.1763   -2.0269    0.1340
+   -0.7559   -2.2200    0.5662   -0.6066
+    1.0752    0.3568    0.8656   -0.7601
+   -0.1220    0.2016   -1.0297    0.6997
+   -1.9932   -0.2445   -1.9596   -0.6715
+    0.1679    1.2502    0.7514    0.4160
+    0.3652    2.1694    0.1697   -1.9544];
+% figure(1)
+% % surf
+% imagesc(a_x(:,1))
+% xlabel('Meaning'), ylabel('Location')%, zlabel('a^{x}')
+% figure(2)
 % surf(v_x)
-%% now obtaining the measurement prob. distribution for the data above (Causal=1)
-like_ax=zeros(n,2,t_max,n_lm);
-like_vx=zeros(n,2,t_max,n_lm);
+% xlabel('Meaning'), ylabel('Location'), zlabel('v^{x}')
+%% we look at one point only first
+prob_ax=zeros(n,m_n);%m_n);
+prob_vx=zeros(n,m_n);%m_n); %we have the dimensions l,m,t,L^{x},M^{x}
+%this will be :,:,t,p,
+%v_x and a_x have dimensions n,2
+% pc=normcdf(p_c,m_s,sig_m_ax);
+% p_c=[1-pc pc]; %prob for m=1 and m=2, note that if the m_x draw is larger than p_c it is put in m=1, thus 1-normcdf is the prob
+% a_x=a_x(1,1,1);
+% prob_ax(3,4)=1
 
-pc=normcdf(p_c,m_s,sig_m_ax);
-p_c=[1-pc pc]; %prob for m=1 and m=2, note that if the m_x draw is larger than p_c it is put in m=1, thus 1-normcdf is the prob
-for q=1:n_lm
-    for o=1:n
-        for p=1:2
-            for t =1:t_max
-                prob_Lvx=normpdf(l(i_l),l(o),sig_lvx); %N(L^x,L^s,sig)
-                prob_vx(o,p,t,q)=prob_Lvx'.*normpdf(v_x(o,p,t,q),mean_rw,sig_e_s).*p_c(p);
-                %prob_vx(i_lv_plt(t,o),m_v(t,o),t,o,p)=p_c(m_v(t,o))*prob_Lvx(i_lv_plt(t,o))*normpdf(v_x(i_lv_plt(t,o),m_v(t,o),t,o,p),mean_rw,sig_e_s);
-    
-                prob_Lax=normpdf(l(i_l),l(o),sig_lax); %N(L^x,L^s,sig)
-                prob_ax(o,p,t,q)=prob_Lax'.*normpdf(a_x(o,p,t,q),mean_rw,sig_e_s).*p_c(p);
-                %prob_ax(i_la_plt(t,o),m_a(t,o),t,o,p)=p_c(m_a(t,o))*prob_Lax(i_la_plt(t,o))*normpdf(a_x(i_la_plt(t,o),m_a(t,o),t,o,p),mean_rw,sig_e_s);
+%when observation is generated from 0
+% mean_rw=0;
+% a_x_val=[-0.8842 1.0951 -0.3416 0.7105 0.5367 2.1711 -1.3945 -0.1956 -0.0428 -0.0646];%a_x(:,1);
+
+%when observation is generated from 10
+% mean_rw=10;
+% a_x_val=[-0.8842 10.0951 -0.3416 0.7105 0.5367 2.1711 -1.3945 -0.1956 -0.0428 -0.0646];%a_x(:,1);
+% L_x=3;
+% L_s=2;
+% figure
+% for l_s_i=1:n
+%     L_s=l(l_s_i);
+% %     disp(L_s)
+%     prob_ax_t=ones(n,1);
+%     prob_Lax=ones(n,1);
+%     for l_x_i=1:n
+%         L_x=l(l_x_i);
+%         prob_Lax(L_x)=normpdf(L_x,L_s,sig_lax);%abs(normcdf(L_x-0.5,L_s,sig_lax)-normcdf(L_x+0.5,L_s,sig_lax));%
+%         for l_i=1:n
+%             if L_x==l_i 
+% %                 disp(L_s)
+% %                 disp("and")                
+% %                 disp(L_x);
+% %                 disp("and")
+% %                 disp(normpdf(a_x_val(l_i),mean_rw,sig_e_s));
+%                 prob_ax_t(l_i)=(normpdf(a_x_val(l_i),mean_rw,sig_e_s));
+%             else
+%                 prob_ax_t(l_i)=(normpdf(a_x_val(l_i),0,sig_e_s));       
+%             end
+%         end
+% %         scatter(L_x,prob_Lax);
+% %         scatter(L_x,(prob_ax_t(l_x_i))); hold on;
+%         disp(prob_ax_t)
+% %         prob_ax_t=prob_Lax*prod(prob_ax_t);
+%         prob_ax(L_s)=sum(prob_Lax*prod(prob_ax_t));
+%     end
+% end
+% m_n=1
+% a_x=a_x(:,2)
+for l_s_i=1:n
+
+    L_s = l(l_s_i);
+    for m_s_i=1:m_n
+        M_s = m(m_s_i);    
+        prob_ax_t=ones(n,m_n,1);
+
+    %     disp(L_s)
+    %     prob_Lax=ones(n,1);
+        for l_x_i=1:n
+            L_x=l(l_x_i);
+            prob_Lax=abs(normcdf(L_x-(l_r/n),L_s,sig_lax)-normcdf(L_x+(l_r/n),L_s,sig_lax));%
+            for m_x_i=1:m_n
+                M_x=m(m_x_i);
+                prob_Max=abs(normcdf(M_x-(m_r/m_n),M_s,sig_m_ax)-normcdf(M_x+(m_r/m_n),M_s,sig_m_ax));%                
+                    for l_i=1:n
+                        for m_i=1:m_n
+                            if l_x_i==l_i && m_x_i==m_i                                
+                %                 disp(L_s)
+                %                 disp("and")                
+                %                 disp(L_x);
+                %                 disp("and")
+                %                 disp(normpdf(a_x_val(l_i),mean_rw,sig_e_s));
+                                prob_ax_t(l_i,m_i)=(normpdf(a_x(l_i,m_i),mean_rw,sig_e_s));
+                            else
+                                prob_ax_t(l_i,m_i)=(normpdf(a_x(l_i,m_i),0,sig_e_s));                                
+                            end
+                        end
+                    end
+        %         scatter(L_x,prob_Lax);
+        %         scatter(L_x,(prob_ax_t(l_x_i))); hold on;
+%                 disp(prob_ax_t)
+        %         prob_ax_t=prob_Lax*prod(prob_ax_t);
+%                 product(:)=(prod(prob_ax_t(:,:),2));
+            product_p_ax(l_x_i,m_x_i)=(prod(prod(prob_ax_t(:,:),2)));
+            prob_m(m_x_i)=prob_Max;
             end
-        end 
+            prob_l(l_x_i)=prob_Lax;
+%             product_ax(l_x_i,m_x_i)=prod(prod(prob_ax_t(:,:),2));
+        end
+%         prob_ax_s=sum(product_s);
+        prob_ax(l_s_i,m_s_i)=sum(sum(prob_l'.*prob_m.*product_p_ax));    
     end
 end
-
-% probax now contains the probability values for 100 different sets of ax,
-% thus we need to sum over these, note that the 5th dimension of the array
-% will represent the different probability sets. If we sum over this
-% dimension we should get a look at the average probability distribution
-% for this value. Expect a peak at the x location in l and m, but less of a
-% peak at the other locations (note that this off course depends on the
-% discrepancy between the noise and the measured). 
-%After this also a sum is performed over all the different options of L_x
-%and M_x that were drawn, resulting in a general shape over the (L,M)-space
-
-figure(1)
-surf(sum(prob_ax(:,:,:,:),4))
-xlabel('Stimulus Location'), ylabel('Stimulus Meaning'), zlabel('Likelihood(stimuli|a^{x})')
-figure(2)
-surf(sum(prob_vx(:,:,:,:),4))
-xlabel('Stimulus Location'), ylabel('Stimulus Meaning'), zlabel('Likelihood(stimuli|a^{x})')
+% hold off;
+if m_n==1 || n==1
+    figure; plot(prob_ax);%
+    figure; plot(a_x);
+else
+    figure; imagesc(prob_ax);%
+    figure; imagesc(a_x);
+end
--- gen_model_full.m
+++ gen_model_full.m
@@ -1,4 +1,4 @@
-function[varargout]=gen_model_full(k,t_max,n,l,varargin)
+function[varargout]=gen_model_full(k,t_max,l,m,varargin)
     % in this code samples from the generative model are obtained, each of the
     % different types of data (loc,meaning,content) is sampled separately for
     % clarity.
@@ -45,10 +45,10 @@ if nargin==20
             c_av_s=[c_av_s c_av_s(i-1)+e_s]; %storing and adding noise
         end
     end
-    a_s=zeros(length(l),2,t_max,n_lm,n_r);
-    v_s=zeros(length(l),2,t_max,n_lm,n_r);
-    a_x=zeros(length(l),2,t_max,n_lm,n_r);
-    v_x=zeros(length(l),2,t_max,n_lm,n_r);
+    a_s=zeros(length(l),length(m),t_max,n_lm,n_r);
+    v_s=zeros(length(l),length(m),t_max,n_lm,n_r);
+    a_x=zeros(length(l),length(m),t_max,n_lm,n_r);
+    v_x=zeros(length(l),length(m),t_max,n_lm,n_r);
     
     for o=1:n_lm
         for i=1:t_max
@@ -73,16 +73,28 @@ if nargin==20
             %making the observations, denoted by x:
             m_av_xa=normrnd(m_s,sig_m_ax);
             m_av_xv=normrnd(m_s,sig_m_vx);
-            if m_av_xa >= p_c
-                m_a(i,o)=1;
-            elseif m_av_xa < p_c
-                m_a(i,o)=2;
-            end
-            if m_av_xv >= p_c
-                m_v(i,o)=1;
-            elseif m_av_xv < p_c
-                m_v(i,o)=2;
-            end
+           
+            [~,d_m_xa]=dsearchn(m_av_xa,m');
+            [~,d_m_xv]=dsearchn(m_av_xv,m'); %getting the distances from the observations to the possible locations
+            
+            i_ma=find(d_m_xa==min(d_m_xa(:)));
+            i_mv=find(d_m_xv==min(d_m_xv(:))); %obtaining the index of the closest matching location
+            
+            m_a(i,o)=(i_ma); %these values represent the result of the delta function from the formulas
+            m_v(i,o)=(i_mv); %these values represent the result of the delta function from the formulas
+            %making the observations, denoted by x:
+%             m_av_xa=normrnd(m_s,sig_m_ax);
+%             m_av_xv=normrnd(m_s,sig_m_vx);
+%             if m_av_xa >= p_c
+%                 m_a(i,o)=1;
+%             elseif m_av_xa < p_c
+%                 m_a(i,o)=2;
+%             end
+%             if m_av_xv >= p_c
+%                 m_v(i,o)=1;
+%             elseif m_av_xv < p_c
+%                 m_v(i,o)=2;
+%             end
         end
     
         % the random walk for the content for the case of C=1
@@ -96,8 +108,8 @@ if nargin==20
                 v_s(i_lv_plt(i,o),m_v(i,o),i)=1*1*f_v_s; %generating the result, and storing it in the auditory/visual observation lists
                 f_a_x=f_a_s+normrnd(0,sig_e_ax);
                 f_v_x=f_v_s+normrnd(0,sig_e_vx); %using the same linear functions as before, but now noise is added
-                a_x(:,:,i,o,p)=normrnd(0,sig_e_ax,length(l),2);
-                v_x(:,:,i,o,p)=normrnd(0,sig_e_vx,length(l),2);
+                a_x(:,:,i,o,p)=normrnd(0,sig_e_ax,length(l),length(m));
+                v_x(:,:,i,o,p)=normrnd(0,sig_e_vx,length(l),length(m));
                 a_x(i_la_plt(i,o),m_a(i,o),i,o,p)=1*1*f_a_x;
                 v_x(i_lv_plt(i,o),m_v(i,o),i,o,p)=1*1*f_v_x; %storing the auditory/visual amplituded in their respective meaing/location bins
             end
@@ -115,98 +127,118 @@ if nargin==20
     varargout{10}=m_av_xv;   
     varargout{11}=m_s;
 end
-if nargin == 6
+if nargin == 24
     sig_l_vs=varargin{1};
     sig_l_as=varargin{2};
     sig_lvx=varargin{3};
     sig_lax=varargin{4};
-    sig_e_s=varargin{5};
-    sig_e_ax=varargin{6};
-    sig_e_vx=varargin{7};
-    p_c=varargin{8};
-    p_h=varargin{9};
-    % location of the stimuli for the case of C=2
-    i_la_plt=zeros(t_max,1);
-    i_lv_plt=zeros(t_max,1);
-    
+    sig_m_s=varargin{5};
+    sig_m_as=varargin{6};
+    sig_m_vs=varargin{7};
+    sig_m_ax=varargin{8};
+    sig_m_vx=varargin{9};
+    sig_e_as=varargin{10};
+    sig_e_vs=varargin{11};
+    sig_e_ax=varargin{12};
+    sig_e_vx=varargin{13};
+    p_c=varargin{14};
+    p_h=varargin{15};
+    n_r=varargin{16};
+    n_lm=varargin{17};
+    mean_rw=varargin{18};
+    % C=2
+    %the code below lets a person estimate a cue for three consecutive
+    %timesteps. For all of these the location of the target remains the same.   
+    % location of the stimuli for the case of C=1
+    i_la_plt=zeros(t_max,n_lm);
+    i_lv_plt=zeros(t_max,n_lm);
     l_i=[0 0 0 0]; %the location about which the original signal is centered
     %generating the signal, denoted by s:   
-    L_av_vs=normrnd(l_i(1),sig_l_vs); %drawing a location and meaning from a prior, this is now a Gaussian
-    [~,d_lv]=dsearchn(L_av_vs,l'); %getting the distances from the observations to the possible locations
-    i_lvs=find(d_lv==min(d_lv(:))); %obtaining the index of the closest matching location
-    
-    L_av_as=normrnd(l_i(1),sig_l_as); %drawing a location and meaning from a prior, this is now a Gaussian
-    [~,d_la]=dsearchn(L_av_as,l'); %getting the distances from the observations to the possible locations
-    i_las=find(d_la==min(d_la(:))); %obtaining the index of the closest matching location
-    for i=1:t_max
-        %making the observations, denoted by x:
-        L_av_xa=normrnd(L_av_as,sig_lax);
-        L_av_xv=normrnd(L_av_vs,sig_lvx);
-       
-        [~,d_l_xa]=dsearchn(L_av_xa,l');
-        [~,d_l_xv]=dsearchn(L_av_xv,l'); %getting the distances from the observations to the possible locations
-        
-        i_la=find(d_l_xa==min(d_l_xa(:)));
-        i_lv=find(d_l_xv==min(d_l_xv(:))); %obtaining the index of the closest matching location
-        
-        i_la_plt(i)=i_la; %these values represent the result of the delta function from the formulas
-        i_lv_plt(i)=i_lv; %these values represent the result of the delta function from the formulas
-    end
-    i_lvs=[l(i_lvs) l(i_lvs) l(i_lvs) l(i_lvs)];
-    i_las=[l(i_las) l(i_las) l(i_las) l(i_las)];
-    
-    % meaning of the stimuli for the case of C=2
-    m_a=zeros(t_max,1);
-    m_v=zeros(t_max,1);
-    m_i=[1 2 3 4];
-    for i=1:t_max
-        m_c=binornd(1,p_c); %getting t_max draws from the binomial distribution determining whether the stimuli should be congruent
-        m_h=binornd(1,p_h); %getting t_max draws from the binomial distribution determining whether the stimuli should be human
-        if m_c==0 && m_h==0
-            m_a(i)=1;
-            m_v(i)=1;
-        elseif m_c==0 && m_h==1
-            m_a(i)=2;
-            m_v(i)=1;
-        elseif m_c==1 && m_h==0
-            m_a(i)=1;
-            m_v(i)=2;
-        elseif m_c==1 && m_h==1
-            m_a(i)=2;
-            m_v(i)=2;
-        end
-    end
+    L_a_s=normrnd(l_i(1),sig_l_as); %drawing a location and meaning from a prior, this is now a Gaussian
+    [~,d_l]=dsearchn(L_a_s,l'); %getting the distances from the observations to the possible locations
+    i_la=find(d_l==min(d_l(:))); %obtaining the index of the closest matching location
 
-    % the random walk for the content for the case of C=2
-    a_s=zeros(length(l),length(m_i),t_max);
-    v_s=zeros(length(l),length(m_i),t_max);
-    a_x=zeros(length(l),length(m_i),t_max);
-    v_x=zeros(length(l),length(m_i),t_max);
-    
-%     c_av_s=[normrnd(5,sig_e_s)]; %drawing c_av^s from the normal distribution
+    L_v_s=normrnd(l_i(1),sig_l_vs); %drawing a location and meaning from a prior, this is now a Gaussian
+    [~,d_l]=dsearchn(L_v_s,l'); %getting the distances from the observations to the possible locations
+    i_lv=find(d_l==min(d_l(:))); %obtaining the index of the closest matching location
+    %the M_s variables
+    m_sa=normrnd(0.5,sig_m_as);
+    m_sv=normrnd(0.5,sig_m_vs);
+
+    m_a=zeros(t_max,n_lm);
+    m_v=zeros(t_max,n_lm);
+    %the c_s variables
     for i=1:t_max
-        e_s=normrnd(10,sig_e_s);
+        e_as=normrnd(mean_rw,sig_e_as);
         if i==1
-            c_av_s=[normrnd(10,sig_e_s)]; %start point of random walk
+            c_a_s=[normrnd(mean_rw,sig_e_as)]; %start point of random walk
         elseif i>1
-            c_av_s=[c_av_s c_av_s(i-1)+e_s]; %storing and adding noise
+            c_a_s=[c_a_s c_a_s(i-1)+e_as]; %storing and adding noise
         end
-        c_av_s=[c_av_s c_av_s(i)+e_s]; %storing and adding noise
-        f_a_s=c_av_s(i);
-%         if i >= t_max/2.5
-        f_v_s=k*c_av_s(i); %applying the linear mapping functions
-%         else
-%             f_v_s=normrnd(0,0.05);
-%         end
-%     end
-        a_s(i_la_plt(i),m_a(i),i)=1*1*f_a_s;  
-        v_s(i_lv_plt(i),m_v(i),i)=1*1*f_v_s; %generating the result, and storing it in the auditory/visual observation lists
-        f_a_x=f_a_s+normrnd(0,sig_e_ax);
-        f_v_x=f_v_s+normrnd(0,sig_e_vx); %using the same linear functions as before, but now noise is added
-        a_x(:,:,i)=normrnd(0,sig_e_ax,length(l),length(m_a));
-        v_x(:,:,i)=normrnd(0,sig_e_vx,length(l),length(m_v));
-        a_x(i_la_plt(i),m_a(i),i)=1*1*f_a_x;
-        v_x(i_lv_plt(i),m_v(i),i)=1*1*f_v_x; %storing the auditory/visual amplituded in their respective meaing/location bins
+        e_vs=normrnd(mean_rw,sig_e_vs);
+        if i==1
+            c_v_s=[normrnd(mean_rw,sig_e_vs)]; %start point of random walk
+        elseif i>1
+            c_v_s=[c_v_s c_v_s(i-1)+e_vs]; %storing and adding noise
+        end
+    end
+    a_s=zeros(length(l),2,t_max,n_lm,n_r);
+    v_s=zeros(length(l),2,t_max,n_lm,n_r);
+    a_x=zeros(length(l),2,t_max,n_lm,n_r);
+    v_x=zeros(length(l),2,t_max,n_lm,n_r);
+    
+    for o=1:n_lm
+        for i=1:t_max
+            %making the observations, denoted by x:
+            L_a_x=normrnd(L_a_s,sig_lax);
+            L_v_x=normrnd(L_av_s,sig_lvx);
+           
+            [~,d_l_xa]=dsearchn(L_a_x,l');
+            [~,d_l_xv]=dsearchn(L_v_x,l'); %getting the distances from the observations to the possible locations
+            
+            i_la=find(d_l_xa==min(d_l_xa(:)));
+            i_lv=find(d_l_xv==min(d_l_xv(:))); %obtaining the index of the closest matching location
+            
+            i_la_plt(i,o)=(i_la); %these values represent the result of the delta function from the formulas
+            i_lv_plt(i,o)=(i_lv); %these values represent the result of the delta function from the formulas
+        end
+    %         i_l=[l(i_l) l(i_l) l(i_l) l(i_l)];
+        
+        % meaning of the stimuli for the case of C=1
+    
+        for i=1:t_max
+            %making the observations, denoted by x:
+            m_a_x=normrnd(m_s,sig_m_ax);
+            m_v_x=normrnd(m_s,sig_m_vx);
+            if m_a_x >= p_c
+                m_a(i,o)=1;
+            elseif m_a_x < p_c
+                m_a(i,o)=2;
+            end
+            if m_v_x >= p_c
+                m_v(i,o)=1;
+            elseif m_v_x < p_c
+                m_v(i,o)=2;
+            end
+        end
+    
+        % the random walk for the content for the case of C=1
+        for p=1:n_r   
+        %     c_av_s=[normrnd(5,sig_e_s)]; %drawing c_av^s from the normal distribution, startpoint of random walk
+            for i=1:t_max
+                f_a_s=c_a_s(i);
+                f_v_s=k*c_v_s(i); %applying the linear mapping functions
+        %     end
+                a_s(i_la_plt(i,o),m_a(i,o),i)=1*1*f_a_s;  
+                v_s(i_lv_plt(i,o),m_v(i,o),i)=1*1*f_v_s; %generating the result, and storing it in the auditory/visual observation lists
+                f_a_x=f_a_s+normrnd(0,sig_e_ax);
+                f_v_x=f_v_s+normrnd(0,sig_e_vx); %using the same linear functions as before, but now noise is added
+                a_x(:,:,i,o,p)=normrnd(0,sig_e_ax,length(l),2);
+                v_x(:,:,i,o,p)=normrnd(0,sig_e_vx,length(l),2);
+                a_x(i_la_plt(i,o),m_a(i,o),i,o,p)=1*1*f_a_x;
+                v_x(i_lv_plt(i,o),m_v(i,o),i,o,p)=1*1*f_v_x; %storing the auditory/visual amplituded in their respective meaing/location bins
+            end
+        end
     end
 
     varargout{1}=i_la_plt;
@@ -215,10 +247,12 @@ if nargin == 6
     varargout{4}=m_v;
     varargout{5}=a_x;
     varargout{6}=v_x;
-    varargout{7}=L_av_vs;
-    varargout{8}=L_av_as;
-    varargout{9}=c_av_s;
-    varargout{10}=m_c;
-    varargout{11}=m_h;
+    varargout{7}=i_la;
+    varargout{8}=i_lv;
+    varargout{9}=c_a_s;    
+    varargout{10}=c_v_s;
+    varargout{11}=m_a_x;
+    varargout{12}=m_v_x;   
+    varargout{13}=m_s;
 end
 end
